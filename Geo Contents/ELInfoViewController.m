11fa5f22814c3f6c7e44b7636a6225a7d79e81f6
Fahied <fahied@gmail.com>
2013-05-23 15:15:59 +0200

    moved infopage to viewDidLoad

--- Geo Contents/ELInfoViewController.m
+++ Geo Contents/ELInfoViewController.m
@@ -28,13 +28,6 @@
 {
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
-    
-}
-
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
     //Create a URL object.
     NSURL *url = [NSURL URLWithString:INFO_PAGE_URL];
     
@@ -43,6 +36,14 @@
     
     //Load the request in the UIWebView.
     [self.infoWebView loadRequest:requestObj];
+
+    
 }
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+    }
 
 @end
