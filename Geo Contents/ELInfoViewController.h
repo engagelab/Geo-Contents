42e593205179e4dc53ef84eedb672812ba575f6a
Fahied <fahied@gmail.com>
2013-05-23 12:48:55 +0200

    created ELInfoViewcontroller

---
+++ Geo Contents/ELInfoViewController.h
@@ -0,0 +1,14 @@
+//
+//  ELInfoViewController.h
+//  Geo Contents
+//
+//  Created by spider on 23.05.13.
+//  Copyright (c) 2013 InterMedia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ELInfoViewController : UIViewController
+@property (strong, nonatomic) IBOutlet UIWebView *infoWebView;
+
+@end
---
+++ Geo Contents/ELInfoViewController.m
@@ -0,0 +1,39 @@
+//
+//  ELInfoViewController.m
+//  Geo Contents
+//
+//  Created by spider on 23.05.13.
+//  Copyright (c) 2013 InterMedia. All rights reserved.
+//
+
+#import "ELInfoViewController.h"
+
+@interface ELInfoViewController ()
+
+@end
+
+@implementation ELInfoViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+    
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
