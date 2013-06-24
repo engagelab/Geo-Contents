5b5ec43c1218ffe24cb3e95d1feeac710af5ff59
Fahied <fahied@gmail.com>
2013-06-24 13:02:31 +0200

    added empty image icon
    formated distance label with intervels i.e m, km

---
+++ Geo Contents/NSString+Distance.h
@@ -0,0 +1,13 @@
+//
+//  NSString+Distance.h
+//  Geo Contents
+//
+//  Created by spider on 24.06.13.
+//  Copyright (c) 2013 InterMedia. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSString (Distance)
+
+@end
---
+++ Geo Contents/NSString+Distance.m
@@ -0,0 +1,13 @@
+//
+//  NSString+Distance.m
+//  Geo Contents
+//
+//  Created by spider on 24.06.13.
+//  Copyright (c) 2013 InterMedia. All rights reserved.
+//
+
+#import "NSString+Distance.h"
+
+@implementation NSString (Distance)
+
+@end
